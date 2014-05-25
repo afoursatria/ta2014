f64719c8a09736cca917fae3bbf9e50a4cf0c5d4
Annisa Prida R <annisa.prida@gmail.com>
2014-05-25 19:20:33 +0700

    layouting footer

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -9,11 +9,44 @@ $this->breadcrumbs=array(
 );
 ?>
 <h1>About</h1>
-
-<p>This is a "static" page. You may change the content of this page
-by updating the file <code><?php echo __FILE__; ?></code>.</p>
-<li class="span3">
-    <a href="#" class="thumbnail" rel="tooltip" data-title="Tooltip">
-        <img src="http://placehold.it/280x180" alt="">
-    </a>
-</li>
+<?php 
+	// $this->widget('bootstrap.widgets.TbThumbnails', array(
+ //    'dataProvider'=>$listDataProvider,
+ //    'template'=>"{items}\n{pager}",
+ //    'itemView'=>'_thumb',
+	// )); 
+?>
+<!-- <p>This is a "static" page. You may change the content of this page
+by updating the file <code><?php echo __FILE__; ?></code>.</p> -->
+<div class = "row">
+	<li class="col-xs-3">
+	    <a href="#" class="thumbnail" rel="tooltip" data-title="Tooltip">
+	        <img src="http://placehold.it/280x180" alt="">
+	    </a>
+	</li>
+	<li class="col-xs-3">
+	    <a href="#" class="thumbnail" rel="tooltip" data-title="Tooltip">
+	        <img src="http://placehold.it/280x180" alt="">
+	    </a>
+	</li>
+	<li class="col-xs-3">
+	    <a href="#" class="thumbnail" rel="tooltip" data-title="Tooltip">
+	        <img src="http://placehold.it/280x180" alt="">
+	    </a>
+	</li>
+	<li class="col-xs-3">
+	    <a href="#" class="thumbnail" rel="tooltip" data-title="Tooltip">
+	        <img src="http://placehold.it/280x180" alt="">
+	    </a>
+	</li>
+	<li class="col-xs-3">
+	    <a href="#" class="thumbnail" rel="tooltip" data-title="Tooltip">
+	        <img src="http://placehold.it/280x180" alt="">
+	    </a>
+	</li>
+	<li class="col-xs-3">
+	    <a href="#" class="thumbnail" rel="tooltip" data-title="Tooltip">
+	        <img src="http://placehold.it/280x180" alt="">
+	    </a>
+	</li>
+</div><!--row-->
