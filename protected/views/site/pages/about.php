ca4fe74f900fef290f692f0122bab64e9c67acde
Annisa Prida R <annisa.prida@gmail.com>
2014-06-04 19:46:12 +0700

    beta about, memberi border radius dan initial content about

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -19,34 +19,82 @@ $this->breadcrumbs=array(
 <!-- <p>This is a "static" page. You may change the content of this page
 by updating the file <code><?php echo __FILE__; ?></code>.</p> -->
 <div class = "row">
-	<li class="col-xs-3">
-	    <a href="#" class="thumbnail" rel="tooltip" data-title="Tooltip">
-	        <img src="http://placehold.it/280x180" alt="">
-	    </a>
+	<li class="col-xs-3 text-center">
+		<div class="row">
+	    <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/annisa.jpeg" alt="" class="img-dev">
+		</div>
+		<div class="row">
+			<ul>
+				<li>Nama</li>
+				<li>Email</li>
+				<li>Institusi</li>
+				<li>Position</li>
+			</ul>
+		</div>
 	</li>
-	<li class="col-xs-3">
-	    <a href="#" class="thumbnail" rel="tooltip" data-title="Tooltip">
-	        <img src="http://placehold.it/280x180" alt="">
-	    </a>
+	<li class="col-xs-3 text-center">
+		<div class="row">
+	    <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/afour.jpeg" alt="" class="img-dev">
+		</div>
+		<div class="row">
+			<ul>
+				<li>Nama</li>
+				<li>Email</li>
+				<li>Institusi</li>
+				<li>Position</li>
+			</ul>
+		</div>
 	</li>
-	<li class="col-xs-3">
-	    <a href="#" class="thumbnail" rel="tooltip" data-title="Tooltip">
-	        <img src="http://placehold.it/280x180" alt="">
-	    </a>
+	<li class="col-xs-3 text-center">
+		<div class="row">
+        <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/ari.jpeg" alt="" class="img-dev">
+		</div>
+		<div class="row">
+			<ul>
+				<li>Nama</li>
+				<li>Email</li>
+				<li>Institusi</li>
+				<li>Position</li>
+			</ul>
+		</div>
 	</li>
-	<li class="col-xs-3">
-	    <a href="#" class="thumbnail" rel="tooltip" data-title="Tooltip">
-	        <img src="http://placehold.it/280x180" alt="">
-	    </a>
+	<li class="col-xs-3 text-center">
+		<div class="row">
+	    <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/arry.jpg" alt="" class="img-dev">
+		</div>
+		<div class="row">
+			<ul>
+				<li>Nama</li>
+				<li>Email</li>
+				<li>Institusi</li>
+				<li>Position</li>
+			</ul>
+		</div>
 	</li>
-	<li class="col-xs-3">
-	    <a href="#" class="thumbnail" rel="tooltip" data-title="Tooltip">
-	        <img src="http://placehold.it/280x180" alt="">
-	    </a>
+	<li class="col-xs-3 text-center">
+		<div class="row">
+        <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/heru.jpg" alt="" class="img-dev">
+		</div>
+		<div class="row">
+			<ul>
+				<li>Nama</li>
+				<li>Email</li>
+				<li>Institusi</li>
+				<li>Position</li>
+			</ul>
+		</div>
 	</li>
-	<li class="col-xs-3">
-	    <a href="#" class="thumbnail" rel="tooltip" data-title="Tooltip">
-	        <img src="http://placehold.it/280x180" alt="">
-	    </a>
+	<li class="col-xs-3 text-center">
+		<div class="row">
+        <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/hilman.jpg" alt="" class="img-dev">
+		</div>
+		<div class="row">
+			<ul>
+				<li>Nama</li>
+				<li>Email</li>
+				<li>Institusi</li>
+				<li>Position</li>
+			</ul>
+		</div>
 	</li>
 </div><!--row-->
