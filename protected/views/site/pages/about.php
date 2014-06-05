f062f5e16ad1ba90f882cb3066be607209d4c8d4
Annisa Prida R <annisa.prida@gmail.com>
2014-06-05 13:33:37 +0700

    pembagian konten about

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -19,82 +19,84 @@ $this->breadcrumbs=array(
 <!-- <p>This is a "static" page. You may change the content of this page
 by updating the file <code><?php echo __FILE__; ?></code>.</p> -->
 <div class = "row">
-	<li class="col-xs-3 text-center">
-		<div class="row">
-	    <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/annisa.jpeg" alt="" class="img-dev">
-		</div>
-		<div class="row">
-			<ul>
-				<li>Nama</li>
-				<li>Email</li>
-				<li>Institusi</li>
-				<li>Position</li>
-			</ul>
-		</div>
-	</li>
-	<li class="col-xs-3 text-center">
-		<div class="row">
-	    <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/afour.jpeg" alt="" class="img-dev">
-		</div>
-		<div class="row">
-			<ul>
-				<li>Nama</li>
-				<li>Email</li>
-				<li>Institusi</li>
-				<li>Position</li>
-			</ul>
-		</div>
-	</li>
-	<li class="col-xs-3 text-center">
-		<div class="row">
-        <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/ari.jpeg" alt="" class="img-dev">
-		</div>
-		<div class="row">
-			<ul>
-				<li>Nama</li>
-				<li>Email</li>
-				<li>Institusi</li>
-				<li>Position</li>
-			</ul>
-		</div>
-	</li>
-	<li class="col-xs-3 text-center">
-		<div class="row">
-	    <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/arry.jpg" alt="" class="img-dev">
-		</div>
-		<div class="row">
-			<ul>
-				<li>Nama</li>
-				<li>Email</li>
-				<li>Institusi</li>
-				<li>Position</li>
-			</ul>
-		</div>
-	</li>
-	<li class="col-xs-3 text-center">
-		<div class="row">
-        <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/heru.jpg" alt="" class="img-dev">
-		</div>
-		<div class="row">
-			<ul>
-				<li>Nama</li>
-				<li>Email</li>
-				<li>Institusi</li>
-				<li>Position</li>
-			</ul>
-		</div>
-	</li>
-	<li class="col-xs-3 text-center">
-		<div class="row">
-        <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/hilman.jpg" alt="" class="img-dev">
-		</div>
-		<div class="row">
-			<ul>
-				<li>Nama</li>
-				<li>Email</li>
-				<li>Institusi</li>
-				<li>Position</li>
-			</ul>
-		</div>
-	</li>
+	<ul class="dev-content">
+		<li class="col-xs-3 dev-list">
+			<div class="row">
+		    <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/annisa.jpeg" alt="" class="img-dev img-responsive text-center">
+			</div>
+			<div class="row">
+				<ul class="dev-detail">
+					<li>Nama</li>
+					<li>Email</li>
+					<li>Institusi</li>
+					<li>Position</li>
+				</ul>
+			</div>
+		</li>
+		<li class="col-xs-3 dev-list">
+			<div class="row">
+		    <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/afour.jpeg" alt="" class="img-dev img-responsive text-center">
+			</div>
+			<div class="row">
+				<ul class="dev-detail">
+					<li>Nama</li>
+					<li>Email</li>
+					<li>Institusi</li>
+					<li>Position</li>
+				</ul>
+			</div>
+		</li>
+		<li class="col-xs-3 dev-list">
+			<div class="row">
+		    <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/ari.jpeg" alt="" class="img-dev img-responsive text-center">
+			</div>
+			<div class="row">
+				<ul class="dev-detail">
+					<li>Nama</li>
+					<li>Email</li>
+					<li>Institusi</li>
+					<li>Position</li>
+				</ul>
+			</div>
+		</li>
+		<li class="col-xs-3 dev-list">
+			<div class="row">
+		    <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/arry.jpg" alt="" class="img-dev img-responsive text-center">
+			</div>
+			<div class="row">
+				<ul class="dev-detail">
+					<li>Nama</li>
+					<li>Email</li>
+					<li>Institusi</li>
+					<li>Position</li>
+				</ul>
+			</div>
+		</li>
+		<li class="col-xs-3 dev-list">
+			<div class="row">
+		    <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/heru.jpg" alt="" class="img-dev img-responsive text-center">
+			</div>
+			<div class="row">
+				<ul class="dev-detail">
+					<li>Nama</li>
+					<li>Email</li>
+					<li>Institusi</li>
+					<li>Position</li>
+				</ul>
+			</div>
+		</li>
+		<li class="col-xs-3 dev-list">
+			<div class="row">
+		    <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/hilman.jpg" alt="" class="img-dev img-responsive text-center">
+			</div>
+			<div class="row">
+				<ul class="dev-detail">
+					<li>Nama</li>
+					<li>Email</li>
+					<li>Institusi</li>
+					<li>Position</li>
+				</ul>
+			</div>
+		</li>
+	</ul>
 </div><!--row-->
