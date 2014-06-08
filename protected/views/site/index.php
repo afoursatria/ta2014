c3adf795197c709717abbb5d0cc5f11cf198db51
Annisa Prida R <annisa.prida@gmail.com>
2014-06-09 03:01:51 +0700

    Memperbaiki yang tidak sinkron layout index

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,25 +1,83 @@
+<?php if(Yii::app()->user->hasFlash('success')):?>
+    <div class="alert alert-danger alert-dismissable">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        <?php echo Yii::app()->user->getFlash('success'); ?>
+    </div>
+<?php endif; ?>
 <?php
 /* @var $this SiteController */
 
-$this->pageTitle=Yii::t('main_layout', 	Yii::app()->name);
+$this->pageTitle=Yii::t('main_layout',  Yii::app()->name);
 ?>
 
-<h3> Top 5 Species </h3>
-<?php 
-    foreach ($topSpecies as $species ) {
-        echo($species->spe_speciesname)."<br />";
-        // echo($species->spe_viewed_count."<br />");
-    }
+<?php
+Yii::app()->clientScript->registerScript('search', "
+$('.search-button').click(function(){
+    $('.search-form').toggle();
+    return false;
+});
+$('.search-form form').submit(function(){
+    $.fn.yiiListView.update('specieslistview', { 
+        //this entire js section is taken from admin.php. w/only this line diff
+        data: $(this).serialize()
+    });
+    return false;
+});
+");
 ?>
+<!--<?php $form=$this->beginWidget('CActiveForm', array(
+        'id'=>'search_form',
+        'enableAjaxValidation'=>false,
+)); ?>
 
-<h3> <?php echo Yii::t('main_data','Top 5 Compound'); ?></h3>
-<?php 
-    foreach ($topCompound as $compound ) {
-        echo($compound->con_contentname)."<br />";
-    }
-?>
+    <?php
+        $category = array(
+            'Species'=>Yii::t('main_data','Species'),
+        'Localname'=>Yii::t('main_data','Localname'),
+        'Virtue'=>Yii::t('main_data','Virtue'),
+        'Contents'=>Yii::t('main_data','Compound'));
+            $options = array(
+                'id' => 'category_id',
+                'ajax' => array('type'=>'POST'
+                                , 'url'=>CController::createUrl('site/updateTextField')
+                                , 'update'=>'#param_id'  //selector to update
+                )
+            );
+        
+        echo CHtml::dropDownList('category_name', '', $category, $options);
+    ?>
 
-<?php echo Yii::t('main_data', 'Browse Search Category')?>
+    <div class="row">
+        <?php 
+            echo CHtml::textField('temp_id','',array('id'=>'param_id')) ;
+        ?>
+    </div>
+
+<?php $this->endWidget(); ?>-->
+
+<!-- <div class="row">
+    <?php 
+    // echo CHtml::dropDownList('Cat','', 
+    //  array(
+    //    'Species'=>Yii::t('main_data','Species'),
+    //    'Localname'=>Yii::t('main_data','Localname'),
+    //    'Virtue'=>Yii::t('main_data','Virtue'),
+    //    'Contents'=>Yii::t('main_data','Compound')),
+    //  array(
+    //    'prompt'=>Yii::t('main_data','Choose Category'),
+  //               'ajax'=>array(
+  //                   'empty'=>'Choose Category',
+  //                   'type'=>'POST',
+  //                   'url' => CController::createUrl('category'),
+  //                   // 'data'=> array('jdok'=>'js:this.value'),
+  //                   'update'=>'#field_search',
+  //                   ))
+    //  ); ?>
+  </div> -->
+
+<div class = "row">
+    <div class="col-xs-3">
+      <?php echo Yii::t('main_data', 'Browse Search Category')?>
 <br />
 
 <?php echo CHtml::link(Yii::t('main_data','Species'), array('species/search'))?>
@@ -33,20 +91,111 @@ $this->pageTitle=Yii::t('main_layout', 	Yii::app()->name);
 <?php echo CHtml::link(Yii::t('main_data','Virtue'), array('Virtue/search'))?>
 <br />
 <br />
+        <div class="spesies clearfix">
+            <div class="element element-10"></div>
+            <p class="text _text text-72">5 Spesies Paling Sering Dicari&nbsp;</p>
+            <div class="element element-12"></div>
+            <ul class="top-search">
+                <?php 
+                    foreach ($topSpecies as $species ) {
+                        echo "<li class='top-list'>".($species->spe_speciesname)."<br /></li>";
+                       // echo($species->spe_viewed_count."<br />");
+                    }
+                ?>
+            </ul>
+            <!-- <div class="element element-12"></div>
+            <p class="text _text text-79">Zephyranthes Tubispatha</p>
+            <div class="element element-13"></div>
+            <p class="text _text text-85">Cloxinia Maculata</p>
+            <div class="element element-14"></div>
+            <p class="text _text text-91">Cloxinia Maculata</p>
+            <div class="element element-15"></div>
+            <p class="text _text text-96">Cloxinia Maculata</p>
+            <div class="element element-17"></div>
+            <p class="text _text text-99">Cloxinia Maculata</p>
+            <div class="element element-18"></div> -->
+        </div> <!--spesies-->
+        <!--senyawa paling dicari-->
+        <div class="senyawa clearfix">
+                <div class="element element-19"></div>
+                <p class="text _text text-112">5 Senyawa Paling Sering Dicari&nbsp;</p>
+                <div class="element element-20"></div>
+                 <ul class="top-search">
+                      <?php 
+                          foreach ($topCompound as $compound ) {
+                              echo "<li class='top-list'>".($compound->con_contentname)."<br /></li>";
+                          }
+                      ?>
+                </ul>
+                <!-- <p class="text _text text-118">Zephyranthes Tubispatha</p>
+                <div class="element element-23"></div>
+                <p class="text _text text-121">Cloxinia Maculata</p>
+                <div class="element element-25"></div>
+                <p class="text _text text-125">Cloxinia Maculata</p>
+                <div class="element element-27"></div>
+                <p class="text _text text-130">Cloxinia Maculata</p>
+                <div class="element element-30"></div>
+                <p class="text _text text-133">Cloxinia Maculata</p>
+                <div class="element element-33"></div> -->
+        </div><!--senyawa-->
+    </div><!--col-xs-4-->
+    <div class = "col-xs-9">
+        <div id = "search-field">
+       <?php 
+    CHtml::beginForm(CHtml::normalizeUrl(array('site/index')), 'get', array('id'=>'filter-form'));
+    echo CHtml::textField('speNameKey', (isset($_GET['speNameKey'])) ? $_GET['speNameKey'] : '', array('id'=>'speNameKey'));
+    echo CHtml::submitButton('Search', array('name'=>'','id'=>'green','class'=>'button','url'=>('species/search')));
+    echo CHtml::link(Yii::t('main_data','Species'), array('species/search'));
+    CHtml::endForm();
+?>
+</div>
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
 
-<?php
-	if(!Yii::app()->user->isGuest)
-		echo Yii::t('main_data','You are logged in as ') . CHtml::link(Yii::app()->user->id, array('user/profile/', 'id'=>Yii::app()->user->getState("no")));
-	else
-		echo CHtml::link(Yii::t('main_layout','Login'), array('login'));
-?> 
-<br />
-<?php 
-	if(Yii::app()->user->getState("role") == null)
-	echo CHtml::link(Yii::t('user','Register'), array('register')); ?>
-<br />
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/slider/1.jpg">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+     <div class="item">
+      <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/slider/2.jpg">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+     <div class="item">
+      <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/slider/3.jpg">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    ...
+  </div>
 
-<?php 
-  if(Yii::app()->user->getState("role") == null)
-  echo CHtml::link(Yii::t('user','Forget Password'), array('resetPassword')); ?>
-<br />
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+  </a>
+</div>
+ 
+        <?php 
+        // $this->widget('zii.widgets.CListView', array(
+        // 'dataProvider'=>$dataProvider,
+        // 'itemView'=>'//species/_view',
+        // 'id'=> 'specieslistview',
+        // )); 
+?>
+    </div>
+</div><!--row-->
+
